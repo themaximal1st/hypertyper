28139b003042cbcb98fbf5099515696d785eb974
The Maximalist <hello@themaximalist.com>
2024-01-24 23:42:14 -0800

    more tests

--- test/hypergraph.test.js
+++ test/hypergraph.test.js
@@ -2,7 +2,6 @@ import Hypergraph from "../src/renderer/src/Hypergraph.js";
 
 import { expect, test } from "vitest";
 
-// TODO: simple single case
 // TODO: test simple A-B-C A-B-D hypergraph
 // TODO: test simple A-B-C C-D-E hypergraph
 // TODO: test simple A-B-C 1-B-2 hypergraph
@@ -36,4 +35,32 @@ test("single hyperedge", () => {
     expect(data.links[1].id).toBe("A.B->A.B.C");
     expect(data.links[1].source).toBe("A.B");
     expect(data.links[1].target).toBe("A.B.C");
+});
+
+test("two distinct hyperedges", () => {
+    const hyperedges = [
+        ["A", "B", "C"],
+        ["1", "2", "3"]
+    ];
+    const hypergraph = new Hypergraph(hyperedges);
+    expect(hypergraph.hyperedges.length).toEqual(2);
+
+    expect(hypergraph.hyperedges[0].symbols).toEqual(["A", "B", "C"]);
+    expect(hypergraph.hyperedges[1].symbols).toEqual(["1", "2", "3"]);
+
+    const data = hypergraph.graphData();
+    expect(data.links.length).toBe(4);
+    expect(data.links[0].id).toBe("A->A.B");
+    expect(data.links[0].source).toBe("A");
+    expect(data.links[0].target).toBe("A.B");
+    expect(data.links[1].id).toBe("A.B->A.B.C");
+    expect(data.links[1].source).toBe("A.B");
+    expect(data.links[1].target).toBe("A.B.C");
+
+    expect(data.links[2].id).toBe("1->1.2");
+    expect(data.links[2].source).toBe("1");
+    expect(data.links[2].target).toBe("1.2");
+    expect(data.links[3].id).toBe("1.2->1.2.3");
+    expect(data.links[3].source).toBe("1.2");
+    expect(data.links[3].target).toBe("1.2.3");
 });
